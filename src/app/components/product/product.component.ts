6134c1f25610cfbe2a7c8fcf9482ca94d6bc3c7f
Haluk Gül <halukgl29@gmail.com>
2021-05-26 01:33:46 +0300

    api connection and cors provided

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
- 
-  products:Product[] = [];
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44335/api/products/getall';
+  // productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // };
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
